95a7efc11fbc737236b091748929752d5c9e0b06
otnipid <ndipinto@protonmail.com>
2024-12-25 15:18:20 -0800

    Converted Markdown components to MDX

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,7 +2,7 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: `**/*.md`,
+  filePathPattern: `**/*.mdx`,
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
@@ -17,4 +17,8 @@ export default makeSource({
   contentDirPath: 'posts',
   documentTypes: [Post],
   disableImportAliasWarning: true,
+  mdx: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
 })
